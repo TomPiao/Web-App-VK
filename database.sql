d250dce203433e1fa4e5d450c518b31065782505
Tom <tom.piao@mail.utoronto.ca>
2018-09-16 15:33:37 -0400

    added some more tables

--- database.sql
+++ database.sql
@@ -1,17 +1,35 @@
 
-CREATE DATABASE VK;
+CREATE DATABASE IF NOT EXISTS VK;
 
 use VK;
 
-CREATE TABLE Appitizers(
+CREATE TABLE IF NOT EXISTS Appitizers(
+    ID varchar(255),
+    Name varchar(255),
+    Price FLOAT,
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE IF NOT EXISTS Soups(
+    ID varchar(255),
+    Name varchar(255),
+    Price FLOAT
+);
+
+CREATE TABLE IF NOT EXISTS PHO(
+    ID varchar(255),
+    Name varchar(255),
+    Price FLOAT
+);
+
+CREATE TABLE IF NOT EXISTS Rice(
     ID varchar(255),
     Name varchar(255),
     Price FLOAT
 );
 
 INSERT INTO Appitizers (ID, Name, Price)
-VALUES ('A1', 'Spring Rolls', 4.50);
+VALUES ('A1', 'Spring Rolls', 4.50),
+('A2', 'Fresh Shredded Pork Rice Paper Roles', 6.50);
 
 SELECT * FROM Appitizers;
-
-DROP DATABASE VK;
